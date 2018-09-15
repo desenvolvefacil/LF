6fd916188ce525d480f401cdcbb33e64cbdcb606
Carlão Martins <carlao84@msn.com>
2018-09-14 23:55:20 -0300

    Add Imgs e menus para UWP (Universal Windows Plataform)

--- LF/LF/Views/MainPage.xaml.cs
+++ LF/LF/Views/MainPage.xaml.cs
@@ -20,11 +20,27 @@ namespace LF.Views
             //da pani nos icones
             //On<Xamarin.Forms.PlatformConfiguration.Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
 
-            Children.Add(new ProdutosPage() { Title = "", Icon = "lanches.png", IdCategoria= Util.CAT_LANCHES_ID});
-            Children.Add(new ProdutosPage() { Title = "", Icon = "bebidas.png", IdCategoria = Util.CAT_BEBIDAS_ID });
-            Children.Add(new ProdutosPage() { Title = "", Icon = "combos.png", IdCategoria = Util.CAT_COMBOS_ID });
-            Children.Add(new MeusPedidosPage() { Title = "", Icon = "pedidos.png" });
-            Children.Add(new CarrinhoPage() { Title = "", Icon = "carrinho.png" });
+            ProdutosPage lanches = new ProdutosPage() { Title = "", Icon = "lanches.png", IdCategoria = Util.CAT_LANCHES_ID };
+            ProdutosPage bebidas = new ProdutosPage() { Title = "", Icon = "bebidas.png", IdCategoria = Util.CAT_BEBIDAS_ID };
+            ProdutosPage combos = new ProdutosPage() { Title = "", Icon = "combos.png", IdCategoria = Util.CAT_COMBOS_ID };
+            MeusPedidosPage pedidos = new MeusPedidosPage() { Title = "", Icon = "pedidos.png" };
+            CarrinhoPage carrinho = new CarrinhoPage() { Title = "", Icon = "carrinho.png" };
+
+
+
+            //Seta o Titulo para windows, já que o UWP não carrega os Icones
+            lanches.On<Xamarin.Forms.PlatformConfiguration.Windows>().Element.Title = "Lanches";
+            bebidas.On<Xamarin.Forms.PlatformConfiguration.Windows>().Element.Title = "Bebidas";
+            combos.On<Xamarin.Forms.PlatformConfiguration.Windows>().Element.Title = "Combos";
+            pedidos.On<Xamarin.Forms.PlatformConfiguration.Windows>().Element.Title = "Meus Pedidos";
+            carrinho.On<Xamarin.Forms.PlatformConfiguration.Windows>().Element.Title = "Carrinhos";
+
+
+            Children.Add(lanches);
+            Children.Add(bebidas);
+            Children.Add(combos);
+            Children.Add(pedidos);
+            Children.Add(carrinho);
 
 
             if (Util.UsuarioLogado != null)
